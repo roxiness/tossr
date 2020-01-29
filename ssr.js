631147f5362d896249d6b202a9ed1902b09277a5
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-01-29 20:53:40 +0100

    mock requestAnimationFrame

--- ssr.js
+++ ssr.js
@@ -1,11 +1,10 @@
 const { JSDOM } = require('jsdom')
 
 module.exports.ssr = async function ssr(template, script, url, eventName = 'app-loaded') {
-    url = url.replace(/^\/\.netlify\/functions\/get-html/, '')
-
     return new Promise(async (resolve, reject) => {
         try {
             const dom = await new JSDOM(template, { runScripts: "outside-only", url: 'http://jsdom.ssr' + url })
+            dom.window.requestAnimationFrame = ()=>{}
             dom.window.eval(script)
             dom.window.addEventListener(eventName, () => {
                 const html = dom.serialize()
