45bbe683c3e11c5723d4d2e9664d323c60a30988
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-01-29 17:28:09 +0100

    close window after serialize

--- ssr.js
+++ ssr.js
@@ -7,7 +7,11 @@ module.exports.ssr = async function ssr(template, script, url, eventName = 'app-
         try {
             const dom = await new JSDOM(template, { runScripts: "outside-only", url: 'http://jsdom.ssr' + url })
             dom.window.eval(script)
-            dom.window.addEventListener(eventName, () => resolve(dom.serialize()))
+            dom.window.addEventListener(eventName, () => {
+                const html = dom.serialize()
+                dom.window.close()
+                resolve(html)
+            })
         } catch (err) {
             throw Error(err)
         }
