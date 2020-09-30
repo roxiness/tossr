2f4af0358567bc6433243f67e2cfff17c0bb8a26
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-09-30 15:13:08 +0200

    add beforeEval to test

--- test/test.js
+++ test/test.js
@@ -2,29 +2,32 @@
 const ssr = require('../ssr').ssr
 const process = require('process');
 
+const script = (function () {
+  const div = document.createElement('div');
+  div.classList.add(window['_myclass'])
+  document.body.appendChild(div);
+  dispatchEvent(new CustomEvent("app-loaded"))
+}).toString()
+
+
 try {
   ssr(`
-<html><head></head><body><canvas></canvas></body></html>`,`
-
-    (function() {
-      const div = document.createElement('div');
-      div.classList.add('loaded')
-      document.body.appendChild(div); 
-      dispatchEvent(new CustomEvent("app-loaded")) 
-    })();
-`,
-    '/').then(html => {
-    const expectedOutput = '<html><head><meta data-render="ssr"></head><body><canvas></canvas><div class="loaded"></div></body></html>';
-    if(html !== expectedOutput) {
+<html><head></head><body><canvas></canvas></body></html>`,
+    `(${script})()`,
+    '/', {
+    beforeEval: dom => dom.window._myclass = 'passed-var'
+  }).then(html => {
+    const expectedOutput = '<html><head><meta data-render="ssr"></head><body><canvas></canvas><div class="passed-var"></div></body></html>';
+    if (html !== expectedOutput) {
       console.error(`output differs from expectation: \n expected:\t${expectedOutput}\n actual:\t${html} `)
       process.exit(1);
     }
     process.exit(0);
   }).catch(e => {
-    console.error('ssr promise error',e);
+    console.error('ssr promise error', e);
     process.exit(1);
   })
-} catch(e) {
-  console.error('ssr call error',e);
+} catch (e) {
+  console.error('ssr call error', e);
   process.exit(1);
 }
